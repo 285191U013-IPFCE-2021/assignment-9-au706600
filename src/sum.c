7edd8754a93ef50f3dfabd1896f9b220bc7e8f6e
Mohammad <mr2018@vghf.com>
2021-11-08 23:22:30 +0100

    implemented sum.c

--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,15 @@
 #include "sum.h"
+#include <assert.h>
 
 int sum(int a[], int n)
 {
-    return 0;
+    /* precondition */ 
+    assert (n>=0); 
+
+    /* postcondition */ 
+    if (n > 0)
+    return a[n - 1] + sum(a, n-1); 
+    else 
+    return 0; 
+
 }

8deef04cfcf15bfc1de2e1ae09536c0d290234d7
au706600 <202106601@post.au.dk>
2021-11-25 22:20:27 +0100

    implemented new search.c

--- src/search.c
+++ src/search.c
@@ -3,13 +3,12 @@
 
 bool search(int a[], int n, int x)
 {
-    if (n = 0)
-    return 0; 
+    if (n == 0)
+    return false; 
 
-    else if (n>0 && a[n - 1] == x)
+    else if ((n>0) && a[n - 1] == x)
     return true;
 
-    else if ((n>0) && (a[n - 1]!= x)); 
-    return search(a, n - 1, x); 
-
+    else if ((n>0) && (a[n - 1]!= x))
+    return search(a,n-1,x);
 }
